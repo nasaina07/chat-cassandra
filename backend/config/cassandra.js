3566f5c6a174782e5c5ae11eee28db7407b7ef63
nasaina07 <nasaina710@gmail.com>
2026-08-17 12:26:14 +0300

    Activer SSL pour Cassandra

--- backend/config/cassandra.js
+++ backend/config/cassandra.js
@@ -9,7 +9,10 @@ const client = new cassandra.Client({
   credentials: { 
     username: process.env.CASSANDRA_USER || 'iccassandra', 
     password: process.env.CASSANDRA_PASSWORD || 'cae7eaa897c28535c925f667693769ed' 
-  } 
+  },
+  sslOptions: {
+    rejectUnauthorized: false
+  }
 });
 
 export async function initTables() {
